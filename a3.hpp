b5b2ebf47991f7e0f10f4d8548b2b2323a26a9db
Sunil Umasankar <sunil.umasankar1995@egmail.com>
2017-12-03 17:25:34 -0500

    added reduce

--- a3.hpp
+++ a3.hpp
@@ -6,6 +6,21 @@
 #ifndef A3_HPP
 #define A3_HPP
 
+// we halve the number of blocks
+__global__ void reduce2(int* gin, int* gout) {
+    extern __shared__ int sdata[];
+    int tid = threadIdx.x;
+    int i = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
+    sdata[tid] = gin[i] + gin[i + blockDim.x];
+    __syncthreads();
+
+    for (int s = blockDim.x >> 1; s > 0; s >>= 1) {
+        if (tid < s) sdata[tid] += sdata[tid + s];
+        __syncthreads();
+    }
+    if (tid == 0) gout[blockIdx.x] = sdata[0];
+}
+
 void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float>& y) {
 
 } // gaussian_kde
