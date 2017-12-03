e47c4ccfe3de5f740c437ec7bee91fc0fc74c7bf
Sunil Umasankar <sunil.umasankar1995@egmail.com>
2017-12-03 17:14:35 -0500

    Initial Commit

---
+++ a3.hpp
@@ -0,0 +1,13 @@
+/*  Sunil
+ *  Umasankar
+ *  suniluma
+ */
+
+#ifndef A3_HPP
+#define A3_HPP
+
+void gaussian_kde(int n, float h, const std::vector<float>& x, std::vector<float>& y) {
+
+} // gaussian_kde
+
+#endif // A3_HPP
